71d76cf0c7158890ddb945e9563ec409e0df6aae
Kennius7 <ogbogukenny@yahoo.com>
2024-10-17 07:50:09 +0100

    touch screen functionality for ball movement

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, ImageBackground, Dimensions } from "react-native";
 import Hello from "../components/hello";
 import { StatusBar } from "expo-status-bar";
@@ -13,6 +13,11 @@ import BottomBoundary from "../components/bottomBoundary";
 
 
 export default function Index() {
+  const [gravityX, setGravityX] = useState(0);
+  const [gravityY, setGravityY] = useState(0);
+  const [moveX, setMoveX] = useState(-1);
+  const [moveY, setMoveY] = useState(-1);
+  const [running, setRunning] = useState(true);
 
   const { width, height } = Dimensions.get("screen");
   const boxSize = Math.trunc(Math.max(width, height) * 0.075);
@@ -20,16 +25,51 @@ export default function Index() {
   const bottomBoundary = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
   let engine = Matter.Engine.create({ enableSleeping: false });
   let world = engine.world;
-  world.gravity.y = 0.04;
+  world.gravity.y = gravityY;
+  world.gravity.x = gravityX;
   Matter.World.add(world, [initialBall, bottomBoundary]);
   // let pos = { x: 200, y: 200 };
   // let initialBall = Matter.Bodies.rectangle(pos.x, pos.y, size.width, size.height, { label: "Ball" });
-  const Physics = (entities, { time }) => {
+  const Physics = (entities, { touches, time, dispatch }) => {
     let engine = entities["physics"].engine;
     Matter.Engine.update(engine, time.delta);
+    touches.filter(t => t.type === "press").forEach(t => {
+      if (moveX === -1 && moveY === -1) {
+        setMoveX(prev => prev * -1);
+        Matter.Body.setVelocity(entities["initialBall"].body, { x: moveX, y: moveY });
+      }
+      if (moveX === 1 && moveY === -1) {
+        setMoveY(prev => prev * -1);
+        Matter.Body.setVelocity(entities["initialBall"].body, { x: moveX, y: moveY });
+      }
+      if (moveX === 1 && moveY === 1) {
+        setMoveX(prev => prev * -1);
+        Matter.Body.setVelocity(entities["initialBall"].body, { x: moveX, y: moveY });
+      }
+      if (moveX === -1 && moveY === 1) {
+        setMoveY(prev => prev * -1);
+        Matter.Body.setVelocity(entities["initialBall"].body, { x: moveX, y: moveY });
+      }
+      // Matter.Body.setVelocity(entities["initialBall"].body, { x: moveX, y: moveY });
+    });
     return entities;
-};
+  };
 
+
+  // useEffect(() => {
+  //   console.log("Looping check...", world.gravity.y, world.gravity.x);
+  //   const gameInterval = setInterval(() => {
+  //     setRunning(!running);
+  //     if (running) {
+  //       setGravityX(prev => prev * -1);
+  //       setGravityY(prev => prev * -1);
+  //     }
+  //   }, 6000);
+
+  //   return () => {
+  //     clearInterval(gameInterval);
+  //   }
+  // }, [gravityX, gravityY, setGravityX, setGravityY, running])
 
 
   return (
@@ -39,6 +79,7 @@ export default function Index() {
     >
       <GameEngine 
         systems={[Physics]}
+        running={running}
         entities={{ 
           physics: { 
             engine: engine, 
